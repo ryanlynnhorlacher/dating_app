a7462dd7c425014158b004cf1c041f8c09fd5563
Ryan Horlacher <horlacherr@gmail.com>
2016-08-19 13:46:16 -0600

    change to message

--- db/migrate/20160819174634_create_messages.rb
+++ db/migrate/20160819174634_create_messages.rb
@@ -2,7 +2,7 @@ class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
       t.string :content, null: false
-      t.string :to, null: false
+      t.string :from, null: false
       t.belongs_to :person
 
       t.timestamps
