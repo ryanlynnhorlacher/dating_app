80d343c4941dcc57c71b2dfc593fc8af1de5fe99
Ryan Horlacher <horlacherr@gmail.com>
2016-08-19 12:12:56 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,9 @@
 Rails.application.routes.draw do
-  get 'messages/index'
+  root to: 'people#index'
 
-  get 'messages/show'
-
-  get 'messages/new'
-
-  get 'messages/edit'
-
-  get 'hobbies/index'
-
-  get 'hobbies/show'
-
-  get 'hobbies/new'
-
-  get 'hobbies/edit'
-
-  get 'addresses/index'
-
-  get 'addresses/show'
-
-  get 'addresses/new'
-
-  get 'addresses/edit'
-
-  get 'people/index'
-
-  get 'people/show'
-
-  get 'people/new'
-
-  get 'people/edit'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :people do
+    resources :addresses
+    resources :hobbies
+    resources :messages
+  end
 end
