336bff4ed6c7fb1db17dc110b0add9efad7d52d8
Dean Joseph <captainwatch98@gmail.com>
2016-08-19 13:47:56 -0600

    people and addresses controllers

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,13 +1,52 @@
 class AddressesController < ApplicationController
+  before_action
+
   def index
+    @addresses = @person.addresses
   end
 
   def show
+
   end
 
   def new
+    @address = Address.new
+  end
+
+  def create
+    @address = @person.addresses.new(address_params)
+    if @person.save
+      redirect_to person_addresses_path(@person, @address)
+    else
+      render :new
+    end
   end
 
   def edit
+
   end
+
+  def update
+
+    if @address.update
+      redirect_to person_address_path(@person, @address)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @address.destroy
+    redirect_to person_address_path(@person, @address)
+  end
+
+  private
+    def address_params
+      params.require(:address).permit(:street_address, :city, :state, :zip)
+    end
+
+    def person
+      @person = Person.find(params[:id])
+    end
+
 end
--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,13 +1,53 @@
 class PeopleController < ApplicationController
+  before_action :person [:show, :create, :edit, :update]
+
   def index
+    @person = Person.all
   end
 
   def show
+
   end
 
   def new
+    @person = Person.new
+  end
+
+  def create
+    @person = Person.new(person_params)
+    if @person.save
+      redirect_to person_path(@person)
+    else
+      render :new
+    end
   end
 
   def edit
+
   end
+
+  def update
+
+    if @person.update
+      redirect_to person_path(@person)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+
+    @person.destroy
+    redirect_to peoples_path
+  end
+
+  private
+    def person_params
+      params.require(:person).permit(:name, :gender, :hair_color, :eye_color, :age, :weight, :height)
+    end
+
+    def person
+      @person = Person.find(params[:id])
+    end
+
 end
